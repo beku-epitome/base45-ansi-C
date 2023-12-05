40334a67f5452af2378b4beae8f127dc2bab6bc8
Benedek Kupper <benedek.kupper@epitome.inc>
2023-12-05 14:56:27 +0100

    clear up antipatterns and formatting

--- base45.c
+++ base45.c
@@ -1,5 +1,5 @@
-#include <ctype.h> 	// for size_t
-#include <string.h> 	// for strlen()
+#include "base45.h"
+#include <string.h> // for strlen()
 
 /*                  Table 1: The Base 45 Alphabet
 
@@ -21,209 +21,216 @@
 
 static const char BASE45_CHARSET[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:";
 
-static unsigned char _C2I[256] = {
-	255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
-	255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
-	36, 255,255,255,  37, 38,255,255, 255,255, 39, 40, 255, 41, 42, 43,
-	 0,   1,  2,  3,   4,  5,  6,  7,   8,  9, 44,255, 255,255,255,255,
+static const unsigned char _C2I[256] = {
+    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+    36,  255, 255, 255, 37,  38,  255, 255, 255, 255, 39,  40,  255, 41,  42,  43,
+    0,   1,   2,   3,   4,   5,   6,   7,   8,   9,   44,  255, 255, 255, 255, 255,
 
-	255, 10, 11, 12,  13, 14, 15, 16,  17, 18, 19, 20,  21, 22, 23, 24, /* uppercase */
-	 25, 26, 27, 28,  29, 30, 31, 32,  33, 34, 35, 35, 255,255,255,255,
-	255, 10, 11, 12,  13, 14, 15, 16,  17, 18, 19, 20,  21, 22, 23, 24, /* lowercase */
-	 25, 26, 27, 28,  29, 30, 31, 32,  33, 34, 35, 35, 255,255,255,255,
+    255, 10,  11,  12,  13,  14,  15,  16,  17,  18,  19,  20,  21,  22,  23,  24, /* uppercase */
+    25,  26,  27,  28,  29,  30,  31,  32,  33,  34,  35,  35,  255, 255, 255, 255,
+    255, 10,  11,  12,  13,  14,  15,  16,  17,  18,  19,  20,  21,  22,  23,  24, /* lowercase */
+    25,  26,  27,  28,  29,  30,  31,  32,  33,  34,  35,  35,  255, 255, 255, 255,
 
-	255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
-	255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
-	255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
-	255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
+    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
 
-	255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
-	255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
-	255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
-	255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
+    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
 };
 
-int base45_encode(char * dst, size_t *_max_dst_len, const unsigned char * src, size_t src_len) {
-  size_t out_len = 0, max_dst_len;
-  max_dst_len = _max_dst_len ? *_max_dst_len : src_len * 4;
-  int i;
+int base45_encode(unsigned char *dst, size_t *_max_dst_len, const unsigned char *src,
+                  size_t src_len)
+{
+    size_t out_len = 0, max_dst_len;
+    max_dst_len = _max_dst_len ? *_max_dst_len : src_len * 4;
+    unsigned i;
 
-  for (i = 0; i < src_len; i+=2) {
-     if (src_len - i > 1) {
-        int x = ((src[i])<<8) + src[i+1];
+    for (i = 0; i < src_len; i += 2) {
+        if (src_len - i > 1) {
+            unsigned x = ((src[i]) << 8) + src[i + 1];
 
-        unsigned char e = x / (45 * 45);
-        x %= 45 * 45;
-        unsigned char d = x / 45;
-        unsigned char c = x % 45;
+            unsigned char e = x / (45 * 45);
+            x %= 45 * 45;
+            unsigned char d = x / 45;
+            unsigned char c = x % 45;
 
-	if (out_len < max_dst_len && dst) {
-		dst[ out_len ] = BASE45_CHARSET[c];
-	}
-	out_len++;
-	if (out_len < max_dst_len && dst) {
-		dst[ out_len ] = BASE45_CHARSET[d];
-	}
-	out_len++;
-	if (out_len < max_dst_len && dst) {
-		dst[ out_len ] = BASE45_CHARSET[e];
-	}
-	out_len++;
-     } else {
-        int x = src[i];
-        unsigned char d = x / 45;
-        unsigned char c = x % 45;
+            if (out_len < max_dst_len && dst) {
+                dst[out_len] = BASE45_CHARSET[c];
+            }
+            out_len++;
+            if (out_len < max_dst_len && dst) {
+                dst[out_len] = BASE45_CHARSET[d];
+            }
+            out_len++;
+            if (out_len < max_dst_len && dst) {
+                dst[out_len] = BASE45_CHARSET[e];
+            }
+            out_len++;
+        } else {
+            unsigned x = src[i];
+            unsigned char d = x / 45;
+            unsigned char c = x % 45;
 
-	if (out_len < max_dst_len && dst) {
-		dst[ out_len ] = BASE45_CHARSET[c];
-	}
-	out_len++;
+            if (out_len < max_dst_len && dst) {
+                dst[out_len] = BASE45_CHARSET[c];
+            }
+            out_len++;
 
-	if (out_len < max_dst_len && dst) {
-		dst[ out_len ] = BASE45_CHARSET[d];
-	}
-	out_len++;
-     }
-  }
-  /* Same non guarantee as strncpy et.al. */
-  if (out_len < max_dst_len && dst) {
-	dst[ out_len ] = 0;
-  }
-
-  if (_max_dst_len) {
-	*_max_dst_len = out_len;
-  }
-  return 0;
-}
-
-int base45_decode(unsigned char * dst, size_t * _max_dst_len, const char *src, size_t src_len) {
-  size_t out_len = 0, max_dst_len;
-  max_dst_len = _max_dst_len  ? *_max_dst_len : src_len;
-  int i;
-
-  if (dst == NULL && _max_dst_len == NULL) {
-	return -2;
-  }
-
-  if (src == NULL) {
-	return -2;
-  }
-
-  if (src_len == 0) {
-	src_len = strlen(src);
-  }
-
-  for(i = 0; i < src_len; i+=3) {
-     int x,a,b;
-
-     if (src_len - i < 2) {
-	return -1;
-     }
-
-     if ((255 == (a = _C2I[(unsigned char)src[i]])) || (255 == (b = _C2I[(unsigned char)src[i+1]]))) {
-	return -1;
-     }
-
-     x = a + 45 * b;
-
-     if (src_len - i >= 3) {
-        if (255 == (a = _C2I[(unsigned char)src[i+2]])) {
-	    return -1;
+            if (out_len < max_dst_len && dst) {
+                dst[out_len] = BASE45_CHARSET[d];
+            }
+            out_len++;
         }
-
-        x += a * 45 * 45;
-
-        if (out_len < max_dst_len && dst) {
-        	dst[out_len] = x / 256;
-        }
-        out_len++;
-	x %= 256;
-    };
-
+    }
+    /* Same non guarantee as strncpy et.al. */
     if (out_len < max_dst_len && dst) {
-        dst[out_len] = x;
+        dst[out_len] = 0;
     }
 
-    out_len++;
-  };
-  if (_max_dst_len) {
-	*_max_dst_len = out_len;
-  }
+    if (_max_dst_len) {
+        *_max_dst_len = out_len;
+    }
+    return 0;
+}
 
-  return 0;
+int base45_decode(unsigned char *dst, size_t *_max_dst_len, const unsigned char *src,
+                  size_t src_len)
+{
+    size_t out_len = 0, max_dst_len;
+    max_dst_len = _max_dst_len ? *_max_dst_len : src_len;
+    unsigned i;
+
+    if (dst == NULL && _max_dst_len == NULL) {
+        return -2;
+    }
+
+    if (src == NULL) {
+        return -2;
+    }
+
+    if (src_len == 0) {
+        src_len = strlen(src);
+    }
+
+    for (i = 0; i < src_len; i += 3) {
+        unsigned x, a, b;
+
+        if (src_len - i < 2) {
+            return -1;
+        }
+
+        if ((255 == (a = _C2I[src[i]])) || (255 == (b = _C2I[src[i + 1]]))) {
+            return -1;
+        }
+
+        x = a + 45 * b;
+
+        if (src_len - i >= 3) {
+            if (255 == (a = _C2I[src[i + 2]])) {
+                return -1;
+            }
+
+            x += a * 45 * 45;
+
+            if (out_len < max_dst_len && dst) {
+                dst[out_len] = x / 256;
+            }
+            out_len++;
+            x %= 256;
+        }
+
+        if (out_len < max_dst_len && dst) {
+            dst[out_len] = x;
+        }
+
+        out_len++;
+    }
+    if (_max_dst_len) {
+        *_max_dst_len = out_len;
+    }
+
+    return 0;
 }
 
 #ifdef BASE45_UTIL
-#include <stdio.h>
-#include <stdlib.h>
+#  include <stdio.h>
+#  include <stdlib.h>
 
-int main(int argc, char ** argv) {
-	FILE * in = stdin;
-	FILE * out = stdout;
-	int decode = 0;
-	int at  = 1;
+int main(int argc, char **argv)
+{
+    FILE *in = stdin;
+    FILE *out = stdout;
+    int decode = 0;
+    int at = 1;
 
-	if (argc > 1 && argv[at][0] == '-') {
-		if (argv[at][1] == 'd') {
-			decode = 1;
-		}
-		else {
-			fprintf(stderr,"Syntax: %s [-d] [infile [outfile]]\n", argv[0]);
-			exit(1);
-		};
-		at++; argc--;
-	};
-	if (argc > 1) {
-		if (NULL == (in = fopen(argv[at],"r"))) {
-			perror("Cannot open input file for reading:");
-			exit(1);
-		};
-		at++; argc--;
-	};
-	if (argc > 1) {
-		if (NULL == (out = fopen(argv[at],"w"))) {
-			perror("Cannot open out file for writing:");
-			exit(1);
-		};
-		at++; argc--;
-	};
+    if (argc > 1 && argv[at][0] == '-') {
+        if (argv[at][1] == 'd') {
+            decode = 1;
+        } else {
+            fprintf(stderr, "Syntax: %s [-d] [infile [outfile]]\n", argv[0]);
+            exit(1);
+        }
+        at++;
+        argc--;
+    }
+    if (argc > 1) {
+        if (NULL == (in = fopen(argv[at], "r"))) {
+            perror("Cannot open input file for reading:");
+            exit(1);
+        }
+        at++;
+        argc--;
+    }
+    if (argc > 1) {
+        if (NULL == (out = fopen(argv[at], "w"))) {
+            perror("Cannot open out file for writing:");
+            exit(1);
+        }
+        at++;
+        argc--;
+    }
 
-#ifdef VALIDATE
-	int i;
-	for(i = 0; i < 45; i++) assert(i ==  _C2I[BASE45_CHARSET[i]]);
-#endif
+#  ifdef VALIDATE
+    int i;
+    for (i = 0; i < 45; i++)
+        assert(i == _C2I[BASE45_CHARSET[i]]);
+#  endif
 
-        while(!feof(in)) {
-        	unsigned char buff[       3 * 1024 ]; // multiple chosen to allow continuation.
-        	unsigned char outbuf[ 3 * 3 * 1024 ];
-		size_t olen = sizeof(outbuf);
-		size_t len = fread(buff, 1, 3 * 1024, in);
+    while (!feof(in)) {
+        unsigned char buff[3 * 1024]; // multiple chosen to allow continuation.
+        unsigned char outbuf[3 * 3 * 1024];
+        size_t olen = sizeof(outbuf);
+        size_t len = fread(buff, 1, 3 * 1024, in);
 
-		buff[len] = 0;
+        buff[len] = 0;
 
-		if (len) {
-			int e;
-			if (decode) {
-				e = base45_decode(outbuf, &olen, (char *) buff, len);
-			}
-			else {
-				e = base45_encode((char *)outbuf, &olen, buff, len);
-			}
+        if (len) {
+            int e;
+            if (decode) {
+                e = base45_decode(outbuf, &olen, (char *)buff, len);
+            } else {
+                e = base45_encode((char *)outbuf, &olen, buff, len);
+            }
 
-			if (e) {
-				fprintf(stderr,"base45 %s failed\n", decode ? "decode" : "encode");
-				exit(1);
-			};
+            if (e) {
+                fprintf(stderr, "base45 %s failed\n", decode ? "decode" : "encode");
+                exit(1);
+            }
 
-			if (olen) {
-				fwrite(outbuf, 1, olen, out);
-			}
-		};
-	}
+            if (olen) {
+                fwrite(outbuf, 1, olen, out);
+            }
+        }
+    }
 
-	fclose(in);
-	fclose(out);
+    fclose(in);
+    fclose(out);
 
-	return(0);
-};
+    return (0);
+}
 #endif
--- test.c
+++ test.c
@@ -5,29 +5,31 @@
 
 #include "base45.h"
 
-void check(char * in, char * out) {
-	char enc[1024];
-	unsigned char dec[1024];
-	size_t elen = sizeof(enc), dlen = sizeof(dec);
-	bzero(enc,1024);
-	bzero(dec,1024);
-	
-	assert(0 == base45_encode(enc, &elen, (const unsigned char*) in, strlen(in)));
-	assert(elen == strlen(out));
-	assert(0 == strcmp(enc,out));
+void check(char *in, char *out)
+{
+    char enc[1024];
+    unsigned char dec[1024];
+    size_t elen = sizeof(enc), dlen = sizeof(dec);
+    bzero(enc, 1024);
+    bzero(dec, 1024);
 
-	assert(0 == base45_decode(dec, &dlen, out, strlen(out)));
-	assert(dlen == strlen(in));
-	assert(0 == bcmp(dec,in,dlen));
+    assert(0 == base45_encode(enc, &elen, (const unsigned char *)in, strlen(in)));
+    assert(elen == strlen(out));
+    assert(0 == strcmp(enc, out));
 
-	printf("base64(\"%s\") -> \"%s\"\n", (char*)dec, enc);
+    assert(0 == base45_decode(dec, &dlen, out, strlen(out)));
+    assert(dlen == strlen(in));
+    assert(0 == bcmp(dec, in, dlen));
+
+    printf("base45(\"%s\") -> \"%s\"\n", (char *)dec, enc);
 }
 
-int main(int argc, char **argv) {
- check("Hello!!","%69 VD92EX0");
- check("base-45","UJCLQE7W581");
- check("ietf!","QED8WEX0");
- check("COVID-19","-M8*+A%R81A6");
- check("2021 Digital Green Certificates for travel",
-	"NF6OF6P34SED-EDAECS34ZKE1$CO345$CBWER.CGPC7WE.OEX.CBJEKWEKEC: C");
+int main(int argc, char **argv)
+{
+    check("Hello!!", "%69 VD92EX0");
+    check("base-45", "UJCLQE7W581");
+    check("ietf!", "QED8WEX0");
+    check("COVID-19", "-M8*+A%R81A6");
+    check("2021 Digital Green Certificates for travel",
+          "NF6OF6P34SED-EDAECS34ZKE1$CO345$CBWER.CGPC7WE.OEX.CBJEKWEKEC: C");
 }
